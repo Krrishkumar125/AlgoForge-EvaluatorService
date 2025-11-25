0bcc3aefa383f2fa2261d5afa67af72e04d08797
Krrishkumar125 <krrishkumar2028@gmail.com>
2025-11-25 15:43:05 +0530

    Feat: Added logic for C++ code in docker

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import express from "express";
 import bullBoardAdapter from "./config/bullBoardConfig.js";
 import logger from "./config/loggerConfig.js";
 import serverConfig from "./config/serverConfig.js";
-import runJava from "./containers/runJavaDocker.js";
+import runCpp from "./containers/runCppDocker.js";
+// import runJava from "./containers/runJavaDocker.js";
 // import runPython from "./containers/runPythonDocker.js";
 import apiRouter from "./routes/index.js";
 import sampleWorker from "./workers/sampleWorker.js";
@@ -33,25 +34,41 @@ app.listen(serverConfig.PORT, () => {
   // y = input()
   // print(int(x) + int(y))`;
 
-  const codeJava = `
-  import java.util.*;
-  public class Main{
-   public static void main(String[] args){
-      Scanner scn = new Scanner(System.in);
-      int x = scn.nextInt();
-      int y = scn.nextInt();
-      System.out.println("1st input value given by user: " + x);
-      System.out.println("2nd input value given by user: " + y);
-      for(int i = 0; i< x; i++){
-         System.out.println(i);
+  // const codeJava = `
+  // import java.util.*;
+  // public class Main{
+  //  public static void main(String[] args){
+  //     Scanner scn = new Scanner(System.in);
+  //     int x = scn.nextInt();
+  //     int y = scn.nextInt();
+  //     System.out.println("1st input value given by user: " + x);
+  //     System.out.println("2nd input value given by user: " + y);
+  //     for(int i = 0; i< x; i++){
+  //        System.out.println(i);
+  //     }
+  //  }
+  // }
+  // `;
+
+  const codeCpp = `
+  #include <iostream>
+  using namespace std;
+  int main() {
+      int x, y;
+      cin >> x >> y;
+      cout << "1st input value given by user: " << x << " ";
+      cout << "2nd input value given by user: " << y << " ";
+      for(int i = 0; i < x; i++) {
+          cout << i << " ";
       }
-   }
+      return 0;
   }
   `;
-
-  const inputCase = `100
+  const inputCase = `10
 200`;
   // runPython(codePython, inputCase);
 
-  runJava(codeJava, inputCase);
+  // runJava(codeJava, inputCase);
+
+  runCpp(codeCpp, inputCase);
 });
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,6 @@
 export const PYTHON_IMAGE = "python:3.8-slim";
 export const JAVA_IMAGE = "eclipse-temurin:17-jdk-jammy";
+export const CPP_IMAGE = "gcc:latest";
 
 //this will represent header size of docker stream
 //docker stream header will contain data about what type of stream i.e stdout/stderr
